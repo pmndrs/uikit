c0991335ea63bbadaef198bd1c7760716c179c8e
Bela Bohlender <bela@bohlender.email>
2024-02-21 01:49:28 +0100

    fix: add google font api key

--- packages/fonts/download.js
+++ packages/fonts/download.js
@@ -5,29 +5,32 @@ const fs = require('fs')
 const [, , fontFamily, variant] = process.argv
 
 https
-  .get(`https://www.googleapis.com/webfonts/v1/webfonts?family=${fontFamily}&key=${process.env.API_KEY}`, (res) => {
-    let body = ''
+  .get(
+    `https://www.googleapis.com/webfonts/v1/webfonts?family=${fontFamily}&key=${process.env.GOOGLE_FONTS_API_KEY}`,
+    (res) => {
+      let body = ''
 
-    res.on('data', (chunk) => {
-      body += chunk
-    })
+      res.on('data', (chunk) => {
+        body += chunk
+      })
 
-    res.on('end', () => {
-      try {
-        let json = JSON.parse(body)
-        if (json.error != null) {
-          console.error('fetch font families', fontFamily, json.error)
-          return
+      res.on('end', () => {
+        try {
+          let json = JSON.parse(body)
+          if (json.error != null) {
+            console.error('fetch font families', fontFamily, json.error)
+            return
+          }
+          const [, url] = Object.entries(json.items[0].files).find(
+            ([name]) => name.toLocaleLowerCase() === variant.toLocaleLowerCase(),
+          )
+          download(url)
+        } catch (error) {
+          console.error('fetch font families', fontFamily, error.message)
         }
-        const [, url] = Object.entries(json.items[0].files).find(
-          ([name]) => name.toLocaleLowerCase() === variant.toLocaleLowerCase(),
-        )
-        download(url)
-      } catch (error) {
-        console.error('fetch font families', fontFamily, error.message)
-      }
-    })
-  })
+      })
+    },
+  )
   .on('error', (error) => {
     console.error('fetch font families', fontFamily, error.message)
   })
