4e09568de3197da34c37451b412d960de9d48237
Bela Bohlender <bela@bohlender.email>
2024-03-01 10:54:07 +0100

    fix: remove mkcert because requires node >= 18

--- examples/dashboard/vite.config.ts
+++ examples/dashboard/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-//@ts-ignore
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   optimizeDeps: {
     include: ['@react-three/uikit-lucide', '@react-three/uikit'],
   },
