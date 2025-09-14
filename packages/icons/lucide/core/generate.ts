0d145db3e57fa3e6b72b04c0dd5390ac5fb7362f
Bela Bohlender <bela@bohlender.email>
2025-09-14 15:42:15 -0700

    fix cicd

--- packages/icons/lucide/core/generate.ts
+++ packages/icons/lucide/core/generate.ts
@@ -13,7 +13,7 @@ async function main() {
     const svg = raw.toString()
     const code = `
       /* eslint-disable no-shadow-restricted-names */
-      import { RenderContext, Svg, SvgProperties, ThreeEventMap, SvgOutProperties, InProperties } from '@pmndrs/uikit'
+      import { RenderContext, Svg, SvgProperties, ThreeEventMap, SvgOutProperties, InProperties, WithSignal } from '@pmndrs/uikit'
       const content = \`${svg}\`;
       export class ${name}Icon<T = {}, EM extends ThreeEventMap = ThreeEventMap, OutProperties extends SvgOutProperties<EM> = SvgOutProperties<EM>> extends Svg<T, EM, OutProperties> {
         constructor(
